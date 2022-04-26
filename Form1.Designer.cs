4bcc1743dfb4ac2be900b491191d0c0d236c0ccc
ekuzhekov99 <ekuzhekov99@gmail.com>
2022-04-26 19:51:30 +0400

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace _2note
+{
+    partial class note
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.newNote = new System.Windows.Forms.Button();
+            this.saveNote = new System.Windows.Forms.Button();
+            this.boldBtn = new System.Windows.Forms.Button();
+            this.italicBtn = new System.Windows.Forms.Button();
+            this.underlineBtn = new System.Windows.Forms.Button();
+            this.strikeoutBtn = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.comboBox2);
+            this.splitContainer1.Panel2.Controls.Add(this.comboBox1);
+            this.splitContainer1.Panel2.Controls.Add(this.strikeoutBtn);
+            this.splitContainer1.Panel2.Controls.Add(this.underlineBtn);
+            this.splitContainer1.Panel2.Controls.Add(this.italicBtn);
+            this.splitContainer1.Panel2.Controls.Add(this.boldBtn);
+            this.splitContainer1.Panel2.Controls.Add(this.saveNote);
+            this.splitContainer1.Panel2.Controls.Add(this.newNote);
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(1011, 689);
+            this.splitContainer1.SplitterDistance = 336;
+            this.splitContainer1.SplitterWidth = 3;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(3, 31);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(383, 626);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // newNote
+            // 
+            this.newNote.Location = new System.Drawing.Point(3, 663);
+            this.newNote.Name = "newNote";
+            this.newNote.Size = new System.Drawing.Size(103, 23);
+            this.newNote.TabIndex = 1;
+            this.newNote.Text = "Новая заметка";
+            this.newNote.UseVisualStyleBackColor = true;
+            this.newNote.Click += new System.EventHandler(this.newNote_Click);
+            // 
+            // saveNote
+            // 
+            this.saveNote.Location = new System.Drawing.Point(311, 663);
+            this.saveNote.Name = "saveNote";
+            this.saveNote.Size = new System.Drawing.Size(75, 23);
+            this.saveNote.TabIndex = 2;
+            this.saveNote.Text = "Сохранить";
+            this.saveNote.UseVisualStyleBackColor = true;
+            this.saveNote.Click += new System.EventHandler(this.saveNote_Click);
+            // 
+            // boldBtn
+            // 
+            this.boldBtn.Location = new System.Drawing.Point(3, 3);
+            this.boldBtn.Name = "boldBtn";
+            this.boldBtn.Size = new System.Drawing.Size(25, 25);
+            this.boldBtn.TabIndex = 3;
+            this.boldBtn.Text = "Ж";
+            this.boldBtn.UseVisualStyleBackColor = true;
+            this.boldBtn.Click += new System.EventHandler(this.boldBtn_Click);
+            // 
+            // italicBtn
+            // 
+            this.italicBtn.Location = new System.Drawing.Point(34, 3);
+            this.italicBtn.Name = "italicBtn";
+            this.italicBtn.Size = new System.Drawing.Size(25, 25);
+            this.italicBtn.TabIndex = 4;
+            this.italicBtn.Text = "К";
+            this.italicBtn.UseVisualStyleBackColor = true;
+            this.italicBtn.Click += new System.EventHandler(this.italicBtn_Click);
+            // 
+            // underlineBtn
+            // 
+            this.underlineBtn.Location = new System.Drawing.Point(65, 3);
+            this.underlineBtn.Name = "underlineBtn";
+            this.underlineBtn.Size = new System.Drawing.Size(25, 25);
+            this.underlineBtn.TabIndex = 5;
+            this.underlineBtn.Text = "Ч";
+            this.underlineBtn.UseVisualStyleBackColor = true;
+            this.underlineBtn.Click += new System.EventHandler(this.underlineBtn_Click);
+            // 
+            // strikeoutBtn
+            // 
+            this.strikeoutBtn.Location = new System.Drawing.Point(96, 3);
+            this.strikeoutBtn.Name = "strikeoutBtn";
+            this.strikeoutBtn.Size = new System.Drawing.Size(39, 25);
+            this.strikeoutBtn.TabIndex = 6;
+            this.strikeoutBtn.Text = "abc";
+            this.strikeoutBtn.UseVisualStyleBackColor = true;
+            this.strikeoutBtn.Click += new System.EventHandler(this.strikeoutBtn_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Arial",
+            "Comic Sans MS",
+            "Georgia",
+            "Impact",
+            "Verdana",
+            "Segoe UI"});
+            this.comboBox1.Location = new System.Drawing.Point(142, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(98, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(246, 4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(98, 21);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // note
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1011, 689);
+            this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "note";
+            this.ShowIcon = false;
+            this.Text = "2note";
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button strikeoutBtn;
+        private System.Windows.Forms.Button underlineBtn;
+        private System.Windows.Forms.Button italicBtn;
+        private System.Windows.Forms.Button boldBtn;
+        private System.Windows.Forms.Button saveNote;
+        private System.Windows.Forms.Button newNote;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _2note
+{
+    public partial class note : Form
+    {
+        public note()
+        {
+            InitializeComponent();
+            //выводим список всех заметок
+            listNotes();
+            // настраиваем кнопки жирного, курсива, подчеркнутого и подчеркнутого текста
+            boldBtn.Font = new Font("Segoe UI", 9, FontStyle.Bold);
+            italicBtn.Font = new Font("Segoe UI", 9, FontStyle.Italic);
+            underlineBtn.Font = new Font("Segoe UI", 9, FontStyle.Underline);
+            strikeoutBtn.Font = new Font("Segoe UI", 9, FontStyle.Strikeout);
+            // размеры
+            splitContainer1.SplitterDistance = 500;
+            richTextBox1.Width = 390;
+            this.Width = 820;
+            foreach (int size in arraySizeFont)
+            {
+                comboBox2.Items.Add(size);
+
+            }
+        }
+
+        int[] arraySizeFont = { 8, 9, 10, 11, 12, 14, 16, 18, 28, 36, 48, 72 };
+        public static List<RichTextBox> TextBoxes = new List<RichTextBox>();
+
+        // данная переменная хранит в себе данные о высоте заметки
+        // которую выводили на прошлой итерации
+        int oldHeight;
+
+        // хранит название файла, который мы открыли для просмотра
+        // в будущем нужно для перезаписи данного файла
+        string nameOpenFile;
+
+        // метод выводит список заметок
+        private void listNotes()
+        {
+            splitContainer1.Panel1.Controls.Clear();
+            TextBoxes.Clear();
+            int height = 150;
+
+            string[] allfiles = Directory.GetFiles(@"..\..\", "*.rtf");
+
+            foreach (string filename in allfiles)
+            {
+                RichTextBox newTextBox = new RichTextBox();
+                RichTextBox lastOldTextBox = TextBoxes.LastOrDefault();
+                Button openButton = new Button();
+                Button deleteButton = new Button();
+
+                // выводим заметку
+                newTextBox.LoadFile(filename);
+                newTextBox.Enabled = false;
+                newTextBox.ScrollBars = RichTextBoxScrollBars.None;
+
+                int textLength = newTextBox.Text.Length;
+                int textLines = newTextBox.GetLineFromCharIndex(textLength) + 1;
+
+                if ((newTextBox.Font.Height * textLines) > height) { newTextBox.Size = new Size(350, height); }
+                else { newTextBox.Size = new Size(350, (newTextBox.Font.Height * textLines) + 20); }
+
+                if (lastOldTextBox == null)
+                {
+                    newTextBox.Location = new Point(10, 40);
+                    openButton.Location = new Point(10, 11);
+                    deleteButton.Location = new Point(284, 11);
+
+
+                }
+                else
+                {
+                    newTextBox.Location = new Point(10, lastOldTextBox.Location.Y + oldHeight + 40);
+                    openButton.Location = new Point(10, lastOldTextBox.Location.Y + oldHeight + 11);
+                    deleteButton.Location = new Point(284, lastOldTextBox.Location.Y + oldHeight + 11);
+                }
+
+                newTextBox.BackColor = Color.FromArgb(187, 187, 187);
+                newTextBox.Cursor = Cursors.Arrow;
+                TextBoxes.Add(newTextBox);
+                splitContainer1.Panel1.Controls.Add(newTextBox);
+                oldHeight = newTextBox.Height;
+
+                string nameBtn = filename.Substring(6, filename.Length - 10);
+
+                // добавляем кнопку открыть
+                openButton.Text = "открыть";
+                openButton.BackColor = Color.White;
+                openButton.FlatAppearance.BorderColor = Color.White;
+                openButton.FlatStyle = FlatStyle.Flat;
+                openButton.Size = new Size(75, 30);
+                openButton.Click += openButtonClick;
+                openButton.Name = nameBtn;
+                splitContainer1.Panel1.Controls.Add(openButton);
+
+                // добавляем кнопку удалить
+                deleteButton.Text = "удалить";
+                deleteButton.BackColor = Color.Red;
+                deleteButton.FlatAppearance.BorderColor = Color.Red;
+                deleteButton.FlatStyle = FlatStyle.Flat;
+                deleteButton.Size = new Size(75, 30);
+                deleteButton.Click += deleteButtonClick;
+                deleteButton.Name = nameBtn;
+                splitContainer1.Panel1.Controls.Add(deleteButton);
+            }
+
+        }
+
+        // открытие заметки
+        private void openButtonClick(object sender, EventArgs eventArgs)
+        {
+            var button = (Button)sender;
+            richTextBox1.LoadFile("..\\..\\" + button.Name + ".rtf");
+            nameOpenFile = button.Name;
+        }
+
+        // удаление заметки
+        private void deleteButtonClick(object sender, EventArgs eventArgs)
+        {
+            var button = (Button)sender;
+            FileInfo fileInf = new FileInfo("..\\..\\" + button.Name + ".rtf");
+            if (fileInf.Exists)
+            {
+                fileInf.Delete();
+            }
+            listNotes();
+            richTextBox1.Clear();
+        }
+
+        // событие ощищается область ввода заметок и удаляет значение переменной
+        private void newNote_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            nameOpenFile = null;
+        }
+
+        // сохранение заметки
+        private void saveNote_Click(object sender, EventArgs e)
+        {
+            // проверка на пустоту, чтобы не хранить пустые файлы
+            if (richTextBox1.Text == "") { MessageBox.Show("Пустая заметка"); }
+            else
+            {
+                // если раньше не открывали заметку
+                // то гененируем новое имя для сохранения файла
+                if (nameOpenFile == null)
+                {
+
+                    Random random = new Random();
+
+                    const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                    string[] allfiles = Directory.GetFiles(@"..\..\", "*.rtf");
+
+                    string name_note = "..\\..\\" + new string(Enumerable.Repeat(chars, 10)
+                      .Select(s => s[random.Next(s.Length)]).ToArray()) + ".rtf";
+
+                    while (allfiles.Contains(name_note) == true)
+                    {
+                        name_note = "..\\..\\" + new string(Enumerable.Repeat(chars, 10)
+                            .Select(s => s[random.Next(s.Length)]).ToArray()) + ".rtf";
+                    }
+                    richTextBox1.SaveFile(name_note);
+                    nameOpenFile = name_note.Substring(6, name_note.Length - 10);
+                }
+
+                else
+                {
+                    string name_note = "..\\..\\" + nameOpenFile + ".rtf";
+                    richTextBox1.SaveFile(name_note);
+                }
+                listNotes();
+            }
+        }
+
+        // сделает текст жирным
+        private void boldBtn_Click(object sender, EventArgs e)
+        {
+                if (richTextBox1.SelectionFont != null)
+                {
+                    Font currentFont = richTextBox1.SelectionFont;
+                    FontStyle newFontStyle;
+
+                    if (richTextBox1.SelectionFont.Bold == true)
+                    {
+                        newFontStyle = FontStyle.Regular;
+                    }
+                    else
+                    {
+                        newFontStyle = FontStyle.Bold;
+                    }
+
+                    richTextBox1.SelectionFont = new Font(
+                    currentFont.FontFamily, currentFont.Size, newFontStyle);
+                }
+          
+        }
+
+        // сделает текст курсивом
+        private void italicBtn_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectionFont != null)
+            {
+                Font currentFont = richTextBox1.SelectionFont;
+                FontStyle newFontStyle;
+
+                if (richTextBox1.SelectionFont.Italic == true)
+                {
+                    newFontStyle = FontStyle.Regular;
+                }
+                else
+                {
+                    newFontStyle = FontStyle.Italic;
+                }
+
+                richTextBox1.SelectionFont = new Font(
+                currentFont.FontFamily, currentFont.Size, newFontStyle);
+            }
+        }
+
+        // подчеркнет текст
+        private void underlineBtn_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectionFont != null)
+            {
+                Font currentFont = richTextBox1.SelectionFont;
+                FontStyle newFontStyle;
+
+                if (richTextBox1.SelectionFont.Underline == true)
+                {
+                    newFontStyle = FontStyle.Regular;
+                }
+                else
+                {
+                    newFontStyle = FontStyle.Underline;
+                }
+
+                richTextBox1.SelectionFont = new Font(
+                currentFont.FontFamily, currentFont.Size, newFontStyle);
+            }
+        }
+
+        // зачеркнет текст
+        private void strikeoutBtn_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectionFont != null)
+            {
+                Font currentFont = richTextBox1.SelectionFont;
+                FontStyle newFontStyle;
+
+                if (richTextBox1.SelectionFont.Strikeout == true)
+                {
+                    newFontStyle = FontStyle.Regular;
+                }
+                else
+                {
+                    newFontStyle = FontStyle.Strikeout;
+                }
+
+                richTextBox1.SelectionFont = new Font(
+                currentFont.FontFamily, currentFont.Size, newFontStyle);
+            }
+        }
+
+        // меняем шрифт
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectionFont != null)
+            {
+                Font currentFont = richTextBox1.SelectionFont;
+                richTextBox1.SelectionFont = new Font(
+                comboBox1.SelectedItem.ToString(), currentFont.Size, currentFont.Style);
+            }
+        }
+        // размер шрифта
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectionFont != null)
+            {
+                Font currentFont = richTextBox1.SelectionFont;
+                richTextBox1.SelectionFont = new Font(
+                currentFont.FontFamily, arraySizeFont[comboBox2.SelectedIndex], currentFont.Style);
+            }
+        }
+    }
+}
